72c26a1352e3f0bc986c5813d6ec5131cd986edf
Elvis <Elvisraphael00@gmail.com>
2024-12-06 22:48:01 +0100

    setting up the structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,27 @@
+import { useReducer } from "react"
+import Quizz from "./Quizz"
+import "./assets/scss/App.scss"
 function App() {
-  return <div>Quizz</div>
+  const [state, shitter] = useReducer(fucker, 0)
+  function fucker(states, bogus) {
+    console.log(states)
+    console.log(bogus)
+    switch (bogus.type) {
+      case "increment":
+        return states + 1
+      case "decrement":
+        return states - 1
+    }
+    // if (action.type == "decrement") return states - action.payload
+    // if (action.type == "increment") return states + action.payload
+  }
+  return (
+    <div>
+      <button onClick={() => shitter({ type: "decrement" })}>-</button>
+      Quizz {state}
+      <button onClick={() => shitter({ type: "increment" })}>+</button>
+      <Quizz />
+    </div>
+  )
 }
 export default App
