161baecb38604927765175163be26004acc32015
Elvis <Elvisraphael00@gmail.com>
2024-12-07 01:18:35 +0100

    question length

--- src/Context.jsx
+++ src/Context.jsx
@@ -1,27 +1,28 @@
 import { createContext, useEffect, useReducer } from "react"
 import QuizzReducer from "./QuizzReducer"
 import axios from "axios"
+
 export const Context = createContext()
+const initialState = {
+  questions: [],
+  answers: [],
+  currentQuestion: 0,
+  status: "load",
+}
 export const QuizzContext = ({ children }) => {
-  const initialState = {
-    questions: [],
-    answers: [],
-    currentQuestion: 0,
-    status: "load",
-  }
   const [state, dispatch] = useReducer(QuizzReducer, initialState)
   useEffect(() => {
     const get = async () => {
       const data = await axios.get("https://the-trivia-api.com/v2/questions")
       dispatch({ type: "data", payload: data.data })
     }
-
     get()
   }, [])
+
   const contextVals = {
     state,
     dispatch,
   }
-  //   console.log(state)
+
   return <Context.Provider value={contextVals}>{children}</Context.Provider>
 }
--- src/Welcome.jsx
+++ src/Welcome.jsx
@@ -1,12 +1,12 @@
 import { useContext } from "react"
 import { Context } from "./Context"
 function Welcome() {
-  const { dispatch } = useContext(Context)
+  const { dispatch, state } = useContext(Context)
   return (
     <div className='welcome'>
       <h1 className='title'>Welcome Quizzical!</h1>
       <p>Start your quiz journey here!</p>
-      <p>X questions are ready for you to answer!</p>
+      <p>{state.questions.length} questions are ready for you to answer!</p>
       <button onClick={() => dispatch({ type: "active" })}>{`Let's Start`}</button>
     </div>
   )
